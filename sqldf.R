6bafbaacb843d28821a6add63005ed2516dcc6de
meniko117 <m_smirnov@yahoo.com>
2019-05-07 21:17:04 +0300

    Update sqldf.R

--- sqldf.R
+++ sqldf.R
@@ -1,37 +1,37 @@
 library(sqldf)
 
-# sqldf íå ðàáîòàåò ñ êîëîíêàìè, â êîòîðûõ ñîäåðæèòñÿ òèï List
-# îïðåäåëèòü òèï êîëîíîê
+# sqldf не работает с колонками, в которых содержится тип List
+# определить тип колонок
 coltypes<-dat2[1,] %>% 
   summarise_all(typeof)
 
-# ïðåîáðàçîâàòü ðÿäû â êîëîíêè
+# преобразовать ряды в колонки
 coltypes<-tidyr::gather(coltypes)
 
 
-# îñòàâëÿåì â òàáëèöå òîëüêî êîëîíêè áåç ïåðåìåííûõ List
+# оставляем в таблице только колонки без переменных List
 datSQL<- dat2[, c(1:23, 25:27, 29:36, 39:42, 46:54, 56:75)]
 
 
-# óáèðàåì "content" èç íàçâàíèé êîëîíîê, ò.ê. sqldf íå èäåíòèôèöèðóåò ýòè íàçâàíèÿ
+# убираем "content" из названий колонок, т.к. sqldf не идентифицирует эти названия
 library(stringr)
 
 colnames(datSQL)<-colnames(datSQL) %>% str_replace("content.", "")
 
-#óáèðàåì äóáëèêàòû â íàçâàíèÿõ
+#убираем дубликаты в названиях
 colnames(datSQL)[27]<-"kktRegId2"
 colnames(datSQL)[43]<-"protocolVersion2" 
 
 
-# ÏÈØÅÌ SQL çàïðîñû
+# ПИШЕМ SQL запросы
 
-# ãðóïïèðóåì ÷åêè ïî ofd
+# группируем чеки по ofd
 View(sqldf("SELECT ofdId, COUNT (ofdId) FROM datSQL GROUP BY ofdId " ))
 
-# cóììà ÷åêà ðàâíà ñóììå íåêòîðûõ ïîëåé ñ ÍÄÑ
+# cумма чека равна сумме некторых полей с НДС
 View(sqldf("SELECT * FROM datSQL WHERE totalSum = nds0 + nds18+ ndsNo " ))
 
-# áîëüøå âñåãî ïðîáèòûõ ÷åêîâ
+# больше всего пробитых чеков
 View(sqldf("SELECT userInn, user, COUNT (userInn) AS cnt FROM datSQL GROUP BY userInn  ORDER BY cnt DESC LIMIT 10" ))
 
 
